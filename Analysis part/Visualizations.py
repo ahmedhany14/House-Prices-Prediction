7956d8ece35080a3ccc8f6804bf07cadf38ea569
Ahmed Hany <ah7608867@gmail.com>
2024-02-11 20:38:54 +0200

    nothing

---
+++ Analysis part/Visualizations.py
@@ -0,0 +1,29 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import time, math
+import scipy.stats as stats
+
+train_df = pd.read_csv(
+    r"/home/ahmed/Ai/Data science and Ml projects/House-Prices-Prediction---Data-Science-Ml-project/original Data sets/train.csv"
+)
+test_df = pd.read_csv(
+    r"/home/ahmed/Ai/Data science and Ml projects/House-Prices-Prediction---Data-Science-Ml-project/original Data sets/test.csv"
+)
+df = train_df.merge(test_df, how="outer")
+
+# price plotting data
+    
+def price_histplot():
+
+    sns.histplot(
+        data=df,
+        x='SalePrice',
+        stat='density',
+        kde=True,
+    )
+    plt.show()
+    return
+
+price_histplot()
