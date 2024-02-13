9967884d86518ee5e4d9648947b43c2d7bc9d046
Ahmed Hany <ah7608867@gmail.com>
2024-02-13 21:27:03 +0200

    add some changes to data clean and change some columns to make the model more stable

---
+++ Data_Cleaning/Data_clening_functions.py
@@ -0,0 +1,49 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.impute import KNNImputer, SimpleImputer
+import math
+
+
+def rename_columns(df=pd.DataFrame):
+    mapper = {
+        "LotArea": "house_area",
+        "OverallQual": "quality",
+        "YearBuilt": "year_built",
+        "YearRemodAdd": "year_remodel_add",
+        "GarageYrBlt": "garage_year_build",
+        "TotalBsmtSF": "basement_area",
+        "GrLivArea": "grade_living_area",
+        "FullBath": 'number_of_bathrooms',
+        'Fireplaces':'has_Fireplaces_or_not',
+        'GarageArea' :'garage_area',
+        'GarageCars':"garage_capacite"
+
+    }
+    df = df.rename(columns=mapper)
+
+    return df
+
+def missing_values(df=pd.DataFrame):
+    columns = []
+
+    for i in df.columns:
+        if df[i].isna().sum() > 0:
+            columns.append(i);
+    
+    simple_imputer = SimpleImputer(missing_values=np.nan, strategy="mean")
+    simple_imputer.fit(df[columns])
+    df[columns] = simple_imputer.transform(df[columns])
+
+    return df
+
+
+
+def remove_outliers_quantile(Q1, Q2, column, df=pd.DataFrame):
+
+    outlier = df[(df[column] < Q1) | (df[column] > Q2)][column]
+
+    column_without_outlier = df[df[column].isin(outlier) == 0]
+    print(len(outlier))
+    return column_without_outlier
--- Machine learning models/Piplines.py
+++ Machine learning models/Piplines.py
@@ -1,1 +1,74 @@
+import pandas as pd
+import numpy as np
+
+import matplotlib.pyplot as plt
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import (
+    StandardScaler,
+    MinMaxScaler,
+    MaxAbsScaler,
+    OneHotEncoder,
+    OrdinalEncoder,
+    FunctionTransformer,
+)
+
+from sklearn.pipeline import Pipeline
+
+from sklearn.linear_model import LinearRegression, Ridge, Lasso
+from sklearn.tree import DecisionTreeRegressor
+
+
+def Columns_transformers(Y=pd.DataFrame()):
+
+    CT = ColumnTransformer(
+        transformers=[
+            (
+                "One Hot Encode",
+                OneHotEncoder(
+                    categories=[
+                        ["Pave", "Grvl"],
+                        ['Floor', 'Wall', 'Grav', 'GasW', 'GasA']
+                    ],
+                    drop="first",
+                ),
+                ["street", 'heating'],
+            ),
+            (
+                "Ordina Encode",
+                OrdinalEncoder(categories=[["N", "Y"], ["Po", "Fa", "TA", "Gd", "Ex"]]),
+                ["central_air_conditioning", "basement_quality"],
+            ),
+        ],
+        remainder="passthrough",
+    )
+
+    y_transformer = FunctionTransformer(func=np.log)
+    
+    y_transformer.fit(Y)
+    
+    return CT, Y
+
+
+class PipLines_:
+
+    def Linear_Standrized_pipline(
+        x_train=pd.DataFrame(),
+        y_train=pd.DataFrame(),
+    ):
+
+        CT, y_train = Columns_transformers(Y=y_train)
+        scaler = StandardScaler()
+        lin_reg = DecisionTreeRegressor(random_state = 100)
+        pip = Pipeline(
+            [
+                ("Columns transformer", CT),
+                ("Standard Scaler", scaler),
+                ("Linear Regression", lin_reg),
+            ]
+        )
+        
+        pip.fit(X=x_train, y=y_train)
+
+        return pip
+
 
